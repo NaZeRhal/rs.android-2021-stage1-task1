1f02fb32a8a260791b29a959c69828a8067fc6d2
NaZeRhal <zhanshi@yandex.ru>
2021-05-27 13:55:07 +0300

    add task solutions

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,26 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val list = mutableListOf<Int>()
+        var copyArray = sadArray
+        while (true) {
+            val initSize = copyArray.size
+            copyArray.indices.forEach { i ->
+                list += copyArray[i]
+                if (list.size > 2) {
+                    val s = list.size - 1
+                    if (list[s - 2] + list[s] < list[s - 1]) {
+                        list.removeAt(s - 1)
+                    }
+                }
+            }
+            copyArray = list.toIntArray()
+            list.clear()
+            if (initSize == copyArray.size) {
+                break
+            }
+        }
+        return copyArray
     }
 }
--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,30 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val out = arrayListOf<String>()
+        val brackets = arrayOf('<', '(', '[', '>', ')', ']')
+        var count: Int
+        var index: Int
+
+        for (i in 0..inputString.lastIndex) {
+            for (bracket in 0 until brackets.size / 2) when (inputString[i]) {
+                brackets[bracket] -> {
+                    index = i + 1
+                    count = 0
+                    loop@ for (j in i..inputString.lastIndex) {
+                        if (inputString[j] == brackets[bracket]) count += 1
+                        if (inputString[j] == brackets[bracket + (brackets.size / 2)]) {
+                            count -= 1
+                            if (count == 0) {
+                                out.add(inputString.substring(index until j))
+                                break@loop
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return out.toTypedArray()
     }
 }
